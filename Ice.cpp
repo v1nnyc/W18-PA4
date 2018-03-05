ba4f1abf4b83dba21a5c6234f2d58e43d65c0661
v1nnyc <cannallav019@yahoo.com>
2018-03-05 15:40:39 -0800

    ahh

--- Ice.cpp
+++ Ice.cpp
@@ -23,40 +23,48 @@ int min(int x, int y){
 }
 
 int ice(TwoD_Array<int> rooms) {
-	TwoD_Array<int> * ice = new TwoD_Array<int>(rooms.getNumRows(), rooms.getNumCols());
-	ice->at(rooms.getNumRows()-1, rooms.getNumCols()-1) = 1;
+	int cols = rooms.getNumCols() - 1;
+	int rows = rooms.getNumRows() - 1;
+	TwoD_Array<int> * netChange = new TwoD_Array<int>(rows + 1, cols + 1);
 	//initialize array to 0
-	for(int i = rooms.getNumRows() - 1; i >= 0; i--){
-		for(int j = rooms.getNumCols() - 1; j>=0; j--){
-			ice->at(i,j)=1;
+	for(int i = rows; i >= 0; i--){
+		for(int j = cols; j>=0; j--){
+			netChange->at(i,j)=0;
 		}
 	}
 
-	ice->at(rooms.getNumRows()-1, rooms.getNumCols()-1) = 1;
+	netChange->at(rows, cols) = 0- rooms.at(rows, cols);
 
-	for(int i = rooms.getNumRows() - 1; i >= 0; i--){
-		for(int j = rooms.getNumCols() - 1; j>=0; j--){
-			int iceAmount = 0;
-			if(ice->at(i,j) + rooms.at(i,j) > 0){
-				iceAmount = ice->at(i,j) + rooms.at(i,j);
+	//netChange->printOut();
+	for(int i = rows; i >= 0; i--){
+		for(int j = cols; j>=0; j--){
+			if(i == rows && j != cols){
+				netChange->at(rows, j) = netChange->at(rows, j+1) - rooms.at(rows, j);
+			}
+			else if(i != rows && j == cols){
+				netChange->at(i, cols) = netChange->at(i+1, cols) - rooms.at(i, cols);
 			}
 			else{
-				iceAmount = 0;
+				int a = netChange->at(i + 1, j) - rooms.at(i, j);
+				int b = netChange->at(i, j + 1) - rooms.at(i, j);
+				netChange->at(i,j) = (abs(a) < abs(b))? a: b;
+				//std::cout<<a<<"\n";
+				//std::cout<<b<<"\n";
+
 			}
-			if(j > 0){
-				ice->at(i, j - 1) = iceAmount; //min(iceAmount, ice->at(i, j - 1));
-			}
-			if(i>0){
-				ice->at(i - 1, j) = iceAmount; //min(iceAmount, ice->at(i-1, j));
-			}
-		ice->printOut();
+		netChange->printOut();
 		}
 		
 	}
 	
-	if(ice->at(0,0)+rooms.at(0,0) < 1)
-		return 1;
+	if(netChange->at(0,0) > 1)
+		if(netChange->at(0,0) > 0 && rooms.at(0,0) < 0)
+			return 1;
+		else
+			return 1 + rooms.at(0,0);
+
 	else
-  		return ice->at(0,0)+rooms.at(0,0);
+  		return 1 - netChange->at(0,0);
+	//return 0;
 }
 #endif
